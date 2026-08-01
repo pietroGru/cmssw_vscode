1892d48cfd83a77aca936fe38a32a393c9ad9852
Pietro Grutta <pietrogrutta@gmail.com>
2026-08-01 14:00:00 +0200

    Including the clangd wrapper

--- clangd/clangd-wrapper.sh
+++ clangd/clangd-wrapper.sh
@@ -2,11 +2,20 @@
 # Example wrapper for clangd
 
 # Load your environment (adapt this line!)
-cd <absolutePathTo_$CMSSWBASE>/src
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_PATH")" && pwd)"   # /eos/home-p/pgrutta/work/stripClust/CMSSW_16_0_0_pre2
+cd $SCRIPT_DIR
 cmsenv 2>&1
 
 # Optionally print for debugging
 # env | grep -E "LD_LIBRARY_PATH|INCLUDE"
 
 # Execute clangd with all passed arguments
-exec clangd "$@"
+exec clangd \
+  --limit-references=100 \
+  --header-insertion=never \
+  --limit-results=20 \
+  -j=1 \
+  --background-index=false \
+  --pch-storage=memory \
+  --compile-commands-dir="$CMSSW_BASE" "$@"
